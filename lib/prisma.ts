8870d0b98fabb56791f07761e7b3238e50b445d5
yash prolix <yash.prolix21@gmail.com>
2026-07-15 15:54:45 +0530

    fix changes.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import 'dotenv/config';
